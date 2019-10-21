51711037d09730c4bfd3f7e280ca71801ac3c59f
Andrew Matheny <androidmatheny@gmail.com>
2019-10-21 02:03:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ def summon_captain_planet(array)
   new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_array)
+  calls_array.any? {|call| call.length>4}
 end
 
 def find_the_cheese# code an argument here
